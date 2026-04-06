918be2dc1c4fd81ec2497b3295fc55d783cfb6e1
Duwiii0 <rafifdwiarka123@gmail.com>
2026-04-06 12:19:46 +0700

    refactor: remove PenilaianSeeder - penilaian dihitung otomatis saat input data mahasiswa

--- app/Database/Seeds/DatabaseSeeder.php
+++ app/Database/Seeds/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call('MahasiswaSeeder');
-        $this->call('PenilaianSeeder');
+        // Penilaian dihitung otomatis saat user input data mahasiswa via form
+        // Bukan di-seed, tapi langsung di-calculate dan disimpan ke penilaian table
     }
 }
